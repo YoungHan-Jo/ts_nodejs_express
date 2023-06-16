48c97cf822820839731934cab696580aade14484
YoungHan Jo <verin4494@gmail.com>
2023-06-16 19:07:44 +0900

    env

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app: Application = express();
 
 const port: number = 3000;
 
 app.get("/toto", (req: Request, res: Response) => {
-  res.send("deploy automatically");
+  res.send(`env: ${process.env.NODE_ENV}, aaa: ${process.env.AAA}`);
 });
 
 app.listen(port, function () {
